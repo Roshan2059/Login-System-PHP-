103a24be22354bc325513e57da2c831555b52e41
Roshan <roshanpanta2059@gmail.com>
2022-04-06 13:14:52 +0545

    fully created

--- _nav.php
+++ _nav.php
@@ -1,4 +1,11 @@
-<!DOCTYPE html>
+<?php
+if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){
+  $loggedin = true; 
+}
+else{
+  $loggedin = false;
+}
+echo '<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -18,19 +25,28 @@
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="home.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="login.php">Log in</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="signup.php">Sign Up</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="logout.php">log out</a>
-        </li>
-      </ul>
+        </li>';
+
+        if(!$loggedin){
+          echo '
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Log in</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="signup.php">Sign Up</a>
+          </li>';
+        }
+
+        if($loggedin){
+          echo '<li class="nav-item">
+            <a class="nav-link" href="logout.php">log out</a>
+          </li>';
+        }
+
+      echo '</ul>
     </div>
   </div>
 </nav>
 </body>
-</html>
+</html>';
+?>
--- home.php
+++ home.php
@@ -1,27 +1,31 @@
 <?php
 session_start();
-if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true){
-    header("location: home.php");
-    exit;
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
+    header("location: login.php");
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome <?php echo $_SESSION['username'];?></title>
-    
+    <title>Welcome <?php echo $_SESSION['username']; ?></title>
+
 </head>
+
 <body>
     <?php
     require '_nav.php';
     ?>
 
     <?php
-    echo "welcome ".$_SESSION['username'];
+    // echo "welcome ".$_SESSION['username'];
+    echo "Hello! " . $_SESSION['username'] . "<br> Where were you? <br>We have  been waiting for you";
     ?>
+
 </body>
+
 </html>
