8faea960953524c063b4b371bf4be14cb56446a8
Roshan <roshanpanta2059@gmail.com>
2022-04-06 18:38:49 +0545

    Probably the final commit but again nothing is perfect in this world

--- home.php
+++ home.php
@@ -13,7 +13,15 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome <?php echo $_SESSION['username']; ?></title>
-
+    <style>
+            .main-container{
+                width: 25%;
+                margin-top: 10vh;
+                margin-left: auto;
+                margin-right: auto;
+                font-size: 30px;
+            }
+    </style>
 </head>
 
 <body>
@@ -21,10 +29,10 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
     require '_nav.php';
     ?>
 
-    <?php
-    // echo "welcome ".$_SESSION['username'];
-    echo "Hello! " . $_SESSION['username'] . "<br> Where were you? <br>We have  been waiting for you";
-    ?>
+    <div class="main-container">
+        <?php echo "Hello! " . $_SESSION['username']; ?>
+        <br>Welcome again.<br> we were waiting for you.
+    </div>
 
 </body>
 
--- login.php
+++ login.php
@@ -25,23 +25,42 @@
 
     <?php
     $login = false;
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         include '_dbConnect.php';
         $username = $_POST['uname'];
         $pass = $_POST['pass'];
 
-        $sql = "select * from `user` where username = '$username' and password = '$pass'";
+        $sql = "select * from `user` where username = '$username'";
         $sqlexe = mysqli_query($conn, $sql);
         $num = mysqli_num_rows($sqlexe);
 
-        if($num == 1){
-            $login = true;
-            session_start();
-            $_SESSION['loggedin'] = true;
-            $_SESSION['username'] = $username;
-            header("location: home.php");
-        }else{
-             echo "invalid credentials";
+        // login when without password hash
+        // if($num == 1){
+        //     $login = true;
+        //     session_start();
+        //     $_SESSION['loggedin'] = true;
+        //     $_SESSION['username'] = $username;
+        //     header("location: home.php");
+        // }else{
+        //      echo "invalid credentials";
+        // }
+
+
+        // login when with password verify function
+        if ($num == 1) {
+            while ($row = mysqli_fetch_assoc($sqlexe)) {
+                if (password_verify($pass, $row['password'])) {
+                    $login = true;
+                    session_start();
+                    $_SESSION['loggedin'] = true;
+                    $_SESSION['username'] = $username;
+                    header("location: home.php");
+                } else {
+                    echo "invalid credentialsss";
+                }
+            }
+        } else {
+            echo "invalid credentials. Please retry again";
         }
     }
     ?>
